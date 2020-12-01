032a47e44df306a3b105197be00d627b38213ae9
Fabian Kloiber <fabiankloiber@gmx.de>
2020-12-01 10:11:13 +0100

    Implement submitting answers, finish day 1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -4,12 +4,38 @@ fn main() -> Result<(), Box<dyn Error>> {
     dotenv::dotenv().ok();
 
     let input = advent_of_code_2020::get_puzzle_input(1)?;
-    let input: Vec<u32> = input
+    let input: Vec<usize> = input
         .lines()
         .map(|l| l.trim().parse())
         .collect::<Result<_, _>>()?;
 
-    println!("{:#?}", input);
+    let mut answer = None;
+    'outer: for (i, a) in input.iter().enumerate() {
+        for b in input[i..].iter() {
+            if a + b == 2020 {
+                answer = Some(a * b);
+                break 'outer;
+            }
+        }
+    }
+
+    let answer = answer.ok_or("answer not found")?;
+    advent_of_code_2020::check_answer(1, 1, answer)?;
+
+    let mut answer = None;
+    'outer_2: for (i, a) in input.iter().enumerate() {
+        for (j, b) in input[i..].iter().enumerate() {
+            for c in input[(i + j)..].iter() {
+                if a + b + c == 2020 {
+                    answer = Some(a * b * c);
+                    break 'outer_2;
+                }
+            }
+        }
+    }
+
+    let answer = answer.ok_or("answer not found")?;
+    advent_of_code_2020::check_answer(1, 2, answer)?;
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,30 +4,105 @@ use std::{
     io::{Read, Write},
 };
 
+use reqwest::blocking::Client;
+
+fn get_client() -> Result<Client, Box<dyn Error>> {
+    use reqwest::header;
+    let mut headers = header::HeaderMap::new();
+    let session = std::env::var("AOC_SESSION_COOKIE")?;
+    headers.insert(
+        header::COOKIE,
+        header::HeaderValue::from_str(&format!("session={}", session))?,
+    );
+    Ok(Client::builder().default_headers(headers).build()?)
+}
+
 pub fn get_puzzle_input(day: u8) -> Result<String, Box<dyn Error>> {
     if day == 0 || day > 25 {
-        Err(format!("day out of range"))?;
+        Err("day out of range")?;
     }
 
-    if let Ok(mut file) = File::open(format!("input/day{}.txt", day)) {
+    let path = format!("input/day{}.txt", day);
+    if let Ok(mut file) = File::open(&path) {
         let mut input = String::new();
         file.read_to_string(&mut input)?;
         return Ok(input);
     }
 
-    let session = std::env::var("AOC_SESSION_COOKIE")?;
-    let client = reqwest::blocking::Client::new();
+    let client = get_client()?;
     let input = client
         .get(&format!("https://adventofcode.com/2020/day/{}/input", day))
-        .header("cookie", format!("session={}", session))
         .send()?
         .text()?;
 
     {
         std::fs::create_dir_all("input")?;
-        let mut file = File::create(format!("input/day{}.txt", day))?;
+        let mut file = File::create(path)?;
         file.write_all(input.as_bytes())?;
     }
 
     Ok(input)
+}
+
+#[derive(serde::Serialize)]
+struct AnswerForm {
+    level: u8,
+    answer: String,
+}
+
+fn submit_answer<S: ToString>(day: u8, level: u8, answer: S) -> Result<bool, Box<dyn Error>> {
+    fn inner(day: u8, level: u8, answer: String) -> Result<bool, Box<dyn Error>> {
+        if day == 0 || day > 25 {
+            Err("day out of range")?;
+        }
+        if level < 1 || level > 2 {
+            Err("level out of range")?;
+        }
+
+        let path = format!("answers/day{}l{}.txt", day, level);
+        if let Ok(mut file) = File::open(&path) {
+            let mut buf = String::new();
+            file.read_to_string(&mut buf)?;
+            return Ok(answer == buf.trim());
+        }
+
+        let client = get_client()?;
+        let response = client
+            .post(&format!("https://adventofcode.com/2020/day/{}/answer", day))
+            .form(&AnswerForm {
+                level,
+                answer: answer.clone(),
+            })
+            .send()?
+            .text()?;
+
+        let is_correct = !response.contains("That's not the right answer.");
+        if is_correct {
+            std::fs::create_dir_all("answers")?;
+            let mut file = File::create(path)?;
+            writeln!(file, "{}", answer)?;
+        }
+
+        Ok(is_correct)
+    }
+
+    inner(day, level, answer.to_string())
+}
+
+pub fn check_answer<S: ToString>(day: u8, level: u8, answer: S) -> Result<(), Box<dyn Error>> {
+    let s_answer = answer.to_string();
+    let is_correct = submit_answer(day, level, answer)?;
+    if is_correct {
+        println!(
+            "day: {}, level: {}; {} is the correct answer.",
+            day, level, s_answer
+        );
+    } else {
+        eprintln!(
+            "day: {}, level: {}; {} is NOT the correct answer.",
+            day, level, s_answer
+        );
+        std::process::exit(1);
+    }
+    Ok(())
 }
